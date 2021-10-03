5491bdba17fe1979731e88d2d49d8896a547e163
ardin-say <91362243+ardin-say@users.noreply.github.com>
2021-10-03 13:02:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const app = express();
+
+
+app.disable('x-powered-by')
+// app.use(express.static('client'));
+
+// //routes
+// app.get('/', (req,res)=>{
+//     res.sendFile(__dirname + '/index.html');
+// });
+
+server = app.listen( process.env.PORT || 5000);
+
+const users = {};
+
+const io = require("socket.io")(server);
+
+io.on('connection' , socket=>{
+	console.log('New User Connected');
+	function ring(){
+		socket.emit('ring-it')
+	}
+
+	socket.on('rec',function(){
+		socket.emit('ring-it')
+	});
+	ring();
+})
+
+console.log(`running on ${server}`);
